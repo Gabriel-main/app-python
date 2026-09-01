e4af5b75ceed9375016353932c1be74c94c38509
gabriel <gcolmenares19@ghotmail.com>
2026-09-01 19:39:32 -0400

    Haciendo cositas

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+from pydoc import text
+
 import flet as ft
+
 def main(page: ft.Page):
+
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.title = "Primera app"
     texto = ft.Text("Hola, Gabriel")
-    page.add(texto)
+    texto2 = ft.Text("Vamos a ver que tal")
+    page.add(texto, texto2)
+
+    def cambiar_texto(e):
+        texto.value = "Vas bien " + texto.value[5:]
+        page.update()
+
+    boton = ft.ElevatedButton("Cambiar texto", on_click=cambiar_texto)
+    page.add(boton)
 
 ft.app(target=main)
